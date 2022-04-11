6cc0e97ccded823bef12d8c5ddad3ddee646df32
lst4rk <olivialistokin@gmail.com>
2022-04-11 09:46:08 -0400

    filled in appendNum function, chooseOperation, and updateDisplay, added event listeners for operation button, clear, delete

--- js/main.js
+++ js/main.js
@@ -9,34 +9,45 @@ class Calculator {
         this.currentOperand = '';
         this.previousOperand = '';
         this.operation = undefined;
+        this.currentOperandTextElement.innerText = '';
+        this.previousOperandTextElement.innerText = '';
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.split('').slice(0, -1).join('');
+        this.updateDisplay();
     }
 
     appendNum(number) {
-        this.currentOperand = number;
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = `${this.currentOperand} ${this.operation} `;
+        this.currentOperand = '';
     }
 
     compute() {
-
+        
     }
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
     } 
 
 }
 
 let numButtons = document.querySelectorAll('[data-number]');
 let operationButtons = document.querySelectorAll('[data-operation]');
-let clearButton = document.querySelectorAll('[data-all-clear]');
-let deleteButton = document.querySelectorAll('[data-delete]');
+let clearButton = document.querySelector('[data-all-clear]');
+let deleteButton = document.querySelector('[data-delete]');
 let equalsButton = document.querySelectorAll('[data-equals]');
 let previousOperandTextElement = document.querySelector('[data-previous-operand]');
 let currentOperandTextElement = document.querySelector('[data-current-operand]');
@@ -46,6 +57,21 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNum(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+clearButton.addEventListener('click', () => {
+    calculator.clear();
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     })
 })
